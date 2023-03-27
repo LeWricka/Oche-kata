5e057ff48d8bda9953bc41f5ced05b699bbca6db
Iker Mariñelarena <iker@540deg.com>
2023-03-27 17:36:53 +0000

    Ohce implementado

--- src/Ohce.php
+++ src/Ohce.php
@@ -4,4 +4,22 @@ namespace Deg540\PHPTestingBoilerplate;
 
 class Ohce
 {
+    // TODO: Ohce Kata
+    public function ohce(string $chain): string
+    {
+        $words = explode(" ", $chain);
+        $name = $words[1];
+        $time = $this->whatTimeIsIt();
+        if($time>=20 && $time<=23 && $time>=0 && $time<6){
+            return "¡Buenas noches ".$name."!";
+        }
+        else if($time>=6 && $time<12){
+            return "¡Buenas tardes ".$name."!";
+        }
+        return "¡Buenos días ".$name."!";
+    }
+
+    public function whatTimeIsIt(): int{
+        return rand(0, 24);
+    }
 }
--- tests/OhceTest.php
+++ tests/OhceTest.php
@@ -5,14 +5,55 @@ declare(strict_types=1);
 namespace Deg540\PHPTestingBoilerplate\Test;
 
 use Deg540\PHPTestingBoilerplate\Ohce;
+use Mockery;
 use PHPUnit\Framework\TestCase;
 
 final class OhceTest extends TestCase
 {
+    private Ohce $ohce;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->ohce = Mockery::mock(Ohce::class);
+    }
+
     /**
      * @test
      */
-    public function ()
+    public function GoodNightGreeting()
     {
+        $this->ohce->allows()->whatTimeIsIt()->andReturn(21);
+        $name = "Pedro";
+
+        $ohce_result = $this->ohce->ohce("ohce ".$name);
+
+        $this->assertEquals("¡Buenas noches ".$name."!", $ohce_result);
+    }
+
+    /**
+     * @test
+     */
+    public function GoodEveningGreeting()
+    {
+        $this->ohce->allows()->whatTimeIsIt()->andReturn(16);
+        $name = "Pedro";
+
+        $ohce_result = $this->ohce->ohce("ohce ".$name);
+
+        $this->assertEquals("¡Buenas tardes ".$name."!", $ohce_result);
+    }
+
+    /**
+     * @test
+     */
+    public function GoodMorningGreeting()
+    {
+        $this->ohce->allows()->whatTimeIsIt()->andReturn(8);
+        $name = "Pedro";
+
+        $ohce_result = $this->ohce->ohce("ohce ".$name);
+
+        $this->assertEquals("¡Buenos días ".$name."!", $ohce_result);
     }
 }
